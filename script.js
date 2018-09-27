5af87417b12dcc4cc4bfdf6df7eae5857f058775
daniilborovkov <daniilborowkow@gmail.com>
2018-09-27 17:18:41 +0600

    Enabled was cookies

--- script.js
+++ script.js
@@ -5,19 +5,17 @@
         // Проверим, есть ли запись в куках о посещении посетителя
         // Если запись есть - ничего не делаем
         if (!$.cookie('was')) {
+            Micromodal.show('modal-1');
         }
-          Micromodal.show('modal-1');
         // Запомним в куках, что посетитель к нам уже заходил
         $.cookie('was', true, {
             expires: 2,
             path: '/'
         });
-
-        $('#db_popup_on_visit_accept').on('click', function () {
-          console.log('Clicked accept');
-          Micromodal.close('modal-1');
-          Micromodal.show('modal-youtube');
+        $('#db_popup_on_visit_accept').on('click', function() {
+            console.log('Clicked accept');
+            Micromodal.close('modal-1');
+            Micromodal.show('modal-youtube');
         })
-        
     })
 }(jQuery, MicroModal));
